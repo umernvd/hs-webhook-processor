2bf352d670f99453331629e37c1166f107472b88
umernvd <muhammadumernaveed1234@gmail.com>
2026-07-14 13:51:57 +0500

    register quote routes in index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const webhookRoutes = require('./webhook.routes');
 const dealRoutes = require('./deal.routes');
+const quoteRoutes = require('./quote.routes');
 const dealController = require('../controllers/deal.controller');
 
 const router = Router();
 
 router.use('/webhook', webhookRoutes);
 router.use('/deals', dealRoutes);
+router.use('/quotes', quoteRoutes);
 router.get('/webhook-events', dealController.getWebhookEvents.bind(dealController));
 
 module.exports = router;
